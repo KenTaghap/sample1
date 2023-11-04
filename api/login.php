efa5d689517f23bde64e401708a0debadb74c03f
KenTaghap <109163116+KenTaghap@users.noreply.github.com>
2023-11-04 23:25:42 +0800

    Add files via upload

---
+++ api/login.php
@@ -0,0 +1,31 @@
+<?php
+require 'vendor/autoload.php';
+
+// Connect to MongoDB Atlas
+$mongoClient = new MongoDB\Client("mongodb+srv://jasondionanao87:Pogiakoxd123@cluster0.kzbqdga.mongodb.net/");
+
+// Select the database and collection
+$database = $mongoClient->Joyicedb;
+$collection = $database->JIp;
+
+$errorMsg = ""; // Initialize an error message variable
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $Username = $_POST["Username"];
+    $Password = $_POST["pass"];
+
+    // Query for the user
+    $query = ["username" => $Username, "password" => $Password];
+    $user = $collection->findOne($query);
+
+    if ($user) {
+        // Successful login, set session variables or redirect to a protected area
+        header("Location: home/index.html");
+        exit();
+    } else {
+         // Invalid login
+    echo "Invalid username or password.";
+    }
+}
+?>
+
